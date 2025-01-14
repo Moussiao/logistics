b93faf01a0cbac126c6c66abed828fd3aea9fb4b
Илья Нестеренко <ilya-nest@yandex.ru>
2025-01-14 15:01:22 +0200

    fix(static): MEDIA_ROOT and STATIC_ROOT added

--- backend/core/settings/media.py
+++ backend/core/settings/media.py
@@ -1,3 +1,7 @@
+import os
+
+from backend.core.settings.common import ROOT_DIR
 from backend.core.settings.environ import env
 
 MEDIA_URL = env("MEDIA_URL", default="/media/")
+MEDIA_ROOT = os.path.join(ROOT_DIR, "www/media")
--- backend/core/settings/static.py
+++ backend/core/settings/static.py
@@ -1,6 +1,10 @@
+import os
+
+from backend.core.settings.common import ROOT_DIR
 from backend.core.settings.environ import env
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = env("STATIC_URL", cast=str, default="/static/")
+STATIC_ROOT = os.path.join(ROOT_DIR, "www/static")
